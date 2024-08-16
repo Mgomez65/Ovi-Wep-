bc95bfb9b3fb2ae9997331ec81f2a2bfa9aad8c3
Matias Gomez <matigomez257@gmail.com>
2024-08-15 21:52:41 -0300

    agrege la confi del server

--- back/server.js
+++ back/server.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require('cors');
+
+
+const app = express()
+
+app.use(express.urlencoded({extends:false}))
+app.use(express.json());
+
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+}));
+
 
 
 dotenv.config({path: './.env'})
 
-const app = express()
 const puerto = process.env.HOST_PUERTO
 
-app.use( '/',(requiere('./rutas/rutas')))
-
+app.use( '/',require("./rutas/rutas"))
+const conexion = require('./dataBase/DB')
 
 app.listen(puerto,(req,res )=>{
     console.log(`Server is running on port ${puerto}`)
