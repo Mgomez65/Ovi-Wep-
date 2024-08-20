394c2d3f301b301cf2b521eb81bbc9cd3cfe400c
walter748 <135364413+Walter748@users.noreply.github.com>
2024-08-20 19:16:28 -0300

    OVI

---
+++ front/src/pages/home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+
+
+    return (
+        <div>
+            <header className="header">OVI</header>
+            <main className="main">
+
+            </main>
+            <footer className="footer">
+                <section>Contactanos</section>
+            </footer>
+        </div>
+    )
+};
+
+export default Home();
