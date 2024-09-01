0f846829d59f82c91c401b219d47b2b3abca60af
walter748 <135364413+Walter748@users.noreply.github.com>
2024-09-01 00:02:28 -0300

    front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Calendario from './pages/calendario';
 import Home from './pages/home';
-/* import CalendarComponent from './components/calendarioPrueba'; */
 import Register from './pages/register'; 
 import Login from './pages/login';
 import Informe from './pages/informe';
@@ -17,7 +16,6 @@ const App = () => {
           <Route path="/home" element={<Home/>} />
           <Route path="/calendario" element={<Calendario/>} />
           <Route path="/informe" element={<Informe/>} />
-          {/* <Route path="/C" element={<CalendarComponent/>} /> */}
         </Routes>
       </Router>
     </>
