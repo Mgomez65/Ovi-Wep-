ceee2733201f2f78ab6f5d73979be03bed3b9b06
Matias Gomez <matigomez257@gmail.com>
2024-09-16 14:32:37 -0300

    agrege gmail

--- back/server.js
+++ back/server.js
@@ -3,6 +3,8 @@ const dotenv = require('dotenv')
 const cors = require('cors');
 const app = express()
 
+const nodemailer = require('nodemailer');
+
 app.use(express.urlencoded({extends:false}))
 app.use(express.json());
 
@@ -12,7 +14,49 @@ app.use(cors({
 }));
 
 
+
 dotenv.config({path: './.env'})
+
+
+const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // true para 465, false para otros puertos
+    auth: {
+      user: 'terrance70@ethereal.email', // Usuario proporcionado por Ethereal
+      pass: 'ed2dGEvMBpNVX2R7Af' // Contraseña proporcionada por Ethereal
+    }
+  });
+  
+  // Ruta para manejar el envío del formulario
+  app.post('/send-email', (req, res) => {
+    const data = req.body;
+  
+    // Configura las opciones del correo
+    const mailOptions = {
+      from: 'terrance70@ethereal.email', // La dirección del remitente
+      to: data.correo, // La dirección del destinatario
+      subject: 'Nuevo mensaje del formulario',
+      text: `
+        Nombre: ${data.nombre}
+        Provincia: ${data.provincia}
+        Empresa: ${data.empresa}
+        Teléfono: ${data.telefono}
+        Cargo: ${data.cargo}
+        Correo: ${data.correo}
+        Comentario: ${data.comentario}
+      `
+    };
+  
+    // Envía el correo
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error al enviar el correo:', error);
+        return res.status(500).send('Error al enviar el correo: ' + error.toString());
+      }
+      res.status(200).send('Correo enviado');
+    });
+  });
 
 
 
@@ -24,3 +68,6 @@ const conexion = require('./dataBase/DB')
 app.listen(process.env.HOST_PUERTO,(req,res )=>{
     console.log(`Server is running on port ${process.env.HOST_PUERTO}`)
 })
+
+
+
