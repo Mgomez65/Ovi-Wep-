c9f00e7b68161895eff856919bf948d0badc9bdd
Mgomez65 <matigomez257@gmail.com>
2024-10-12 20:37:05 -0300

    agregue para que se pueda subir imagen al back

--- back/src/informe/informe.routes.js
+++ back/src/informe/informe.routes.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const controllerinforme = require("./controller/informe.controller")
+const controllerinforme = require("./controller/informe.controller");
+const multer = require('multer');
+const path = require('path');
 
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
 
+const upload = multer({ storage: storage });
 
-//rutas de genacion de pdf
+// Rutas
 router.get('/descargar/:idInforme', controllerinforme.downloadPDF);
+router.post("/create", upload.single('imagen'), controllerinforme.createIforme);
+router.get("/users", controllerinforme.getInforme);
+router.get("/user/:id", controllerinforme.getInformeId);
+router.delete("/delete/:id", controllerinforme.deleteInforme);
+router.put("/update/:id", controllerinforme.updateInforme);
+router.post("/search", controllerinforme.searchInforme);
 
-
-router.post("/create",controllerinforme.createIforme)
-
-router.get("/users",controllerinforme.getInforme)
-router.get("/user/:id",controllerinforme.getInformeId)
-
-router.delete("/delete/:id",controllerinforme.deleteInforme)
-router.put("/update/:id",controllerinforme.updateInforme)
-
-
-router.post("/search", controllerinforme.searchInforme)
-
-
-
-module.exports = router
-
-
-
+module.exports = router;
